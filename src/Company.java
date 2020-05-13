e8012fb4fb5e36c5c8e007314057c109bcb93f51
Reginaldo-cct <reginaldosantus@gmail.com>
2020-05-13 22:39:38 +0100

    3- Class Investor add getters and setters plus display details... need to be checked and corrections. Company class added showing details

--- src/Company.java
+++ src/Company.java
@@ -10,6 +10,14 @@ class Company {
 			this.CompanyID = id;
 			this.NumberOfShares = (int) (Math.random()*(1000 - 500) + 500); // 500 - 1000
 			this.SharePrice = (int) (Math.random()*(100 - 10) + 10); // 10 - 100
+		
+		}
+		public void DisplayCompany() {// Showing Company details .....
+			
+			System.out.println("\n > Company ID : " + this.CompanyID);
+			System.out.println(" > Company Shares Number : " + this.NumberOfShares);
+			System.out.println(" > Company Share Price : " + this.SharePrice);
+			
 		}
 		
 }
--- src/Investor.java
+++ src/Investor.java
@@ -12,5 +12,39 @@ class Investor
 		this.Shares = 0;
 	
 	}
+
+	public int getInvestorID() {
+		return InvestorID;
+	}
+
+	public void setInvestorID(int investorID) {
+		InvestorID = investorID;
+	}
+
+	public int getBudget() {
+		return Budget;
+	}
+
+	public void setBudget(int budget) {
+		Budget = budget;
+	}
+
+	public int getShares() {
+		return Shares;
+	}
+
+	public void setShares(int shares) {
+		Shares = shares;
+	}
+	
+	
+	public void DisplayInvestor() // Showing Invetor details .....
+	{
+		System.out.println("\n > Investor ID : " + this.InvestorID);
+		System.out.println(" > Investor Budget : " + this.Budget);
+		System.out.println(" > Investor Shares : " + this.Shares);
+	
+}
+	
 	
 }
