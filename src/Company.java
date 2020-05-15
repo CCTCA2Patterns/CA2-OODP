988cf90eaed468f93c0ec9dc89437cbd2f40c6cc
Fernando Aires <fgustavo.aires@gmail.com>
2020-05-15 22:20:46 +0100

    5. Commit Company Capital/Shares/Prices

--- src/Company.java
+++ src/Company.java
@@ -1,45 +1,75 @@
 
-class Company {
+class Company
+{
+	private int CompanyID;
+	private int NumberOfShares;
+	private int SharePrice;
 	
-		private int CompanyID;
-		private int NumberOfShares;
-		private int SharePrice;
-		
-		public Company(int id)
+	public Company(int id)
+	{
+		this.CompanyID = id;
+		this.NumberOfShares = (int) (Math.random()*(1000 - 500) + 500); // 500 - 1000
+		this.SharePrice = (int) (Math.random()*(100 - 10) + 10); // 10 - 100
+	}
+	
+	public void ReduceSharePrice() // When no share sold .... 
+	{
+		int percent = (this.SharePrice * 2) / 100;
+		this.SharePrice = this.SharePrice - percent;
+	}
+	
+	public void DoubleSharePrice() // When sold 10 shares ......
+	{
+		this.SharePrice = this.SharePrice * 2;
+	}
+	
+	public int Capital() // To rank it ..
+	{
+		return this.NumberOfShares * this.SharePrice;
+	}
+	
+	public void DisplayCompany() // Showing Company details .....
+	{
+		System.out.println("\n > Company ID : " + this.CompanyID);
+		System.out.println(" > Company Shares Number : " + this.NumberOfShares);
+		System.out.println(" > Company Share Price : " + this.SharePrice);
+	}
+	
+	public int SellShare() // Selling share any random between 1-10
+	{
+		int share = (int) (Math.random()*(10) + 1); // Any random share MAXIMUM 20 ...
+		this.NumberOfShares = this.NumberOfShares - share;
+		return share;
+	}
+	
+	public int GetNumberOfShares()
+	{
+		return this.NumberOfShares;
+	}
+	
+	public int GetSharePrice()
+	{
+		return this.SharePrice;
+	}
+	
+	public int GetCompanyID()
+	{
+		return this.CompanyID;
+	}
+	
+	public boolean IsEmptyShares() // If company shares are ended .....
+	{
+		if(this.NumberOfShares <= 0)
 		{
-			this.CompanyID = id;
-			this.NumberOfShares = (int) (Math.random()*(1000 - 500) + 500); // 500 - 1000
-			this.SharePrice = (int) (Math.random()*(100 - 10) + 10); // 10 - 100
-		
+			return true;
 		}
-		public int Capital() // To rank it ..
-		{
-			return this.NumberOfShares * this.SharePrice;
-		}
-		public void DisplayCompany() {// Showing Company details .....
-			
-			System.out.println("\n > Company ID : " + this.CompanyID);
-			System.out.println(" > Company Shares Number : " + this.NumberOfShares);
-			System.out.println(" > Company Share Price : " + this.SharePrice);
-			
-		}
-		public int getCompanyID() {
-			return CompanyID;
-		}
-		public void setCompanyID(int companyID) {
-			CompanyID = companyID;
-		}
-		public int getNumberOfShares() {
-			return NumberOfShares;
-		}
-		public void setNumberOfShares(int numberOfShares) {
-			NumberOfShares = numberOfShares;
-		}
-		public int getSharePrice() {
-			return SharePrice;
-		}
-		public void setSharePrice(int sharePrice) {
-			SharePrice = sharePrice;
-		}
-		
+		return false;
+	}
+	
+	public boolean IsAbleToShare()  // If COmpany has enough shares to sell ......
+	{
+		if(this.NumberOfShares <= 0)
+			return false;
+		return true;
+	}
 }
