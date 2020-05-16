93a8fa0267a6201aff78d96b5c6b4422152c5ef0
Reginaldo-cct <reginaldosantus@gmail.com>
2020-05-16 20:40:56 +0100

    6 - Maind Methos add commands for botton...Calling Simulations functions

--- src/Driver.java
+++ src/Driver.java
@@ -1,21 +1,8 @@
+import java.util.Scanner;
 
 public class Driver {
-
-	public static void main(String[] args) {
 	
-		// Creating 100 Companies Dynamically ....
-			Company []companies;
-			companies=new Company[100];
-	        for (int i=0;i<100;i++)
-	        	companies[i]= new Company(i+1+100);
-	        
-	        // Creating 100 Investors Dynamically ....
-	        Investor []investors;
-	        investors =new Investor[100];
-	        for (int i=0;i<100;i++)
-	            investors[i]= new Investor(i+1+100);
-
-	}
+	
 	public static void HighestCapital(Company []obj)
 	{
 		int highest = obj[0].Capital();
@@ -47,5 +34,95 @@ public class Driver {
 		System.out.println("\n\t\t///// Lowest Capital Company /////");
 		obj[loc].DisplayCompany();
 	}
+	
+	
+	
+	
+	
+	
+	
+	// FERNANDO ADD HERE COMAPNY AND INVESTOR BOTTONS 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	//////////////////////Main Function////////////////////
+	public static void main(String[] args) {
+	
+		// Creating 100 Companies Dynamically ....
+			Company []companies;
+			companies=new Company[100];
+	        for (int i=0;i<100;i++)
+	        	companies[i]= new Company(i+1+100);
+	        
+	        // Creating 100 Investors Dynamically ....
+	        Investor []investors;
+	        investors =new Investor[100];
+	        for (int i=0;i<100;i++)
+	            investors[i]= new Investor(i+1+2000);
+	        
+	     // Calling simulation function ...........
+	        Simulator(companies, investors);
+	        
+	        // Menu After Simulation stooped ..........
+	        while(true)
+	        {
+	         	System.out.println("\n\n\t\t //// SIMULATION RESULTS /////\n");
+	        	System.out.println("\n\t\t >> ( Press 1 ) for Compay with Highest Capital");
+	        	System.out.println("\t\t >> ( Press 2 ) for Compay with Lowest Capital");
+	        	System.out.println("\t\t >> ( Press 3 ) for Compay Position");
+	        	System.out.println("\n\t\t >> ( Press 4 ) for Investor with Highest Shares");
+	        	System.out.println("\t\t >> ( Press 5 ) for Investor with Lowest Shares");
+	        	System.out.println("\t\t >> ( Press 6 ) for Investor Position");
+	        	System.out.println("\t\t >> ( Press 0 ) for Exit Program");
+	        	System.out.println("\n\t\t >> Your Choice : ");
+	        	Scanner input = new Scanner(System.in);
+	        	int choice = input.nextInt();
+	        
+	        	switch(choice)
+	        	{
+	        	case 1: HighestCapital(companies);
+	        		break;
+	        	case 2: LowestCapital(companies);
+	        		break;
+	        	case 3: CompanyPosition(companies);
+	        		break;
+	        	case 4: HighestShare(investors);
+	        		break;
+	        	case 5: LowestShare(investors);
+	        		break;
+	        	case 6: InvestorPosition(investors);
+	        		break;
+	        	case 0: 
+	        		break;
+	        	default:
+	        		System.out.println("\n\n\t >> Invalid Choice ...."); break;
+	        	}
+	        	if(choice == 0)
+	        		break;
+	        }
+	        
+	        System.out.println("\n\n\t >> Program Exit ....");
+	        
+	        
+	        
+
+	}
+	
 }
 	
--- src/Investor.java
+++ src/Investor.java
@@ -1,18 +1,20 @@
 
-class Investor
-{
+class Investor {
+	
+	// investor informations 
+	private int Shares;
 	private int InvestorID;
 	private int Budget;
-	private int Shares;
+	
 	
 	public Investor(int id)
 	{
 		this.InvestorID = id;
-		this.Budget = (int) (Math.random()*(10000 - 1000) + 1000);
+		this.Budget = (int) (Math.random()*(10000 - 1000) + 1000);//budget math
 		this.Shares = 0;
 
 	}
-
+	//method updating Shares
 	public void UpdateShates( int shares) {
 		this.Shares = shares;
 	}
@@ -29,15 +31,35 @@ class Investor
 		return this.Budget;
 	}
 	
-	
-	public void DisplayInvestor() // Showing Invetor details .....
-	{
+	public void DisplayInvestor()	{ // Showing Invetor details .....
+		//print the informations of Investor
 		System.out.println("\n > Investor ID : " + this.InvestorID);
 		System.out.println(" > Investor Budget : " + this.Budget);
 		System.out.println(" > Investor Shares : " + this.Shares);
 	
 }
 	public void InvestAmount(int amount) { // Deduct amount from budget .....
+		
+		this.Budget = this.Budget - amount;
+		if(this.Budget < 0)
+		this.Budget = 0;
+		
+	}
+	
+	public void PurchaseShare(int shares) // Adding up shares .....
+	{
+		this.Shares = this.Shares + shares;
 	
 	}
+	public boolean IsEmptyBudget() // If budget is ended ...
+	{
+		if(this.Budget <= 0)
+		{
+			return true;
+		}
+		return false;
+	}
+	
 }
+
+
